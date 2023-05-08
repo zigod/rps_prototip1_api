a13812eeaae1537d276c8dfa13c96415f96e0fdf
Gasper Novak <gaspernovak@proton.me>
2023-05-08 19:46:57 +0200

    Implemented jwt

---
+++ middlewares/authJwt.js
@@ -0,0 +1,24 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const User = require("../models/usersModel");
+
+verifyToken = (req, res, next) => {
+    let token = req.headers["x-access-token"];
+    if (!token) {
+        return res.status(403).send({ message: "No token!" });
+    }
+
+    jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: "Unauthorized!" });
+        }
+        req.userId = decoded.id;
+        next();
+    });
+};
+
+const authJwt = {
+    verifyToken,
+};
+
+module.exports = authJwt;
